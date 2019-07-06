24a14fa36b166a6d15fb25a1a7b3dd89892b6f70
Tatiana <tmedeiross@globo.com>
2019-07-06 15:04:22 -0300

    projeto

---
+++ src/routes.js
@@ -0,0 +1,16 @@
+import { Router } from 'express';
+import User from './app/models/User';
+
+const routes = new Router();
+
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Tatiana',
+    email: 'teste@teste',
+    password_hash: '123456',
+  });
+
+  return res.json(user);
+});
+
+export default routes;
